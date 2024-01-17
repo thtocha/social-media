4ff8b2952e029d0489704475fda010dc3a908c46
thtocha <max.tatochenko@gmail.com>
2024-01-17 20:33:27 +0200

    Add uploading profile details

--- resources/js/Pages/Profile/View.vue
+++ resources/js/Pages/Profile/View.vue
@@ -66,21 +66,12 @@
                     </div>
                     <div class="flex flex-1 justify-between items-center p-4">
                         <h2 class="font-bold text-lg">{{user.name}}</h2>
-                        <PrimaryButton v-if="isMyProfile">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-                            </svg>
-                            Edit profile
-                        </PrimaryButton>
                     </div>
                 </div>
             </div>
             <div class="border-t">
                 <TabGroup>
                     <TabList class="flex bg-white">
-                        <Tab v-if="isMyProfile" v-slot="{ selected }" as="template">
-                            <TabItem text="About" :selected="selected"></TabItem>
-                        </Tab>
                         <Tab v-slot="{ selected }" as="template">
                             <TabItem text="Posts" :selected="selected"></TabItem>
                         </Tab>
@@ -93,12 +84,12 @@
                         <Tab v-slot="{ selected }" as="template">
                             <TabItem text="Photos" :selected="selected"></TabItem>
                         </Tab>
+                        <Tab v-if="isMyProfile" v-slot="{ selected }" as="template">
+                            <TabItem text="About" :selected="selected"></TabItem>
+                        </Tab>
                     </TabList>
 
                     <TabPanels class="mt-2">
-                        <TabPanel v-if="isMyProfile">
-                            <Edit :must-verify-email="mustVerifyEmail" :status="status"></Edit>
-                        </TabPanel>
                         <TabPanel  class="bg-white p-3 shadow">
                             Posts
                         </TabPanel>
@@ -110,6 +101,9 @@
                         </TabPanel>
                         <TabPanel  class="bg-white p-3 shadow">
                             Photos
+                        </TabPanel>
+                        <TabPanel v-if="isMyProfile">
+                            <Edit :must-verify-email="mustVerifyEmail" :status="status"></Edit>
                         </TabPanel>
                     </TabPanels>
                 </TabGroup>
