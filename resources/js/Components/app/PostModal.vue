97237367fc02a74c792019b16102060ea363a964
thtocha <max.tatochenko@gmail.com>
2024-01-20 14:44:28 +0200

    Add ckeditor in post update modal

--- resources/js/Components/app/PostModal.vue
+++ resources/js/Components/app/PostModal.vue
@@ -41,7 +41,8 @@
                             </DialogTitle>
                             <div class="p-4">
                                 <PostUserHeader :post="post" :show-time="false" class="mb-4"/>
-                                <InputTextarea v-model="form.body" class="mb-3 w-full" />
+                                <ckeditor :editor="editor" v-model="form.body" :config="editorConfig"></ckeditor>
+<!--                                <InputTextarea v-model="form.body" class="mb-3 w-full" />-->
                             </div>
 
                             <div class="py-3 px-4">
@@ -69,6 +70,12 @@ import {TransitionRoot, TransitionChild, Dialog, DialogPanel, DialogTitle,} from
 import InputTextarea from "@/Components/InputTextarea.vue";
 import PostUserHeader from "@/Components/app/PostUserHeader.vue";
 import {useForm} from "@inertiajs/vue3";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+const editor = ClassicEditor;
+const editorConfig = {
+    toolbar: ['heading', '|', 'bold', 'italic', '|', 'link', 'bulletedList', 'numberedList', '|', 'outdent', 'indent', '|', 'blockQuote']
+}
 
 const props = defineProps({
     post: {
