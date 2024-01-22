61f958a434969f3779f88a31e15905bdbdb450bf
thtocha <max.tatochenko@gmail.com>
2024-01-22 13:55:51 +0200

    Implement post creating through PostModal

--- resources/js/Components/app/CreatePost.vue
+++ resources/js/Components/app/CreatePost.vue
@@ -3,49 +3,47 @@
 
 import {ref} from "vue";
 import InputTextarea from "@/Components/InputTextarea.vue";
-import {router, useForm} from "@inertiajs/vue3";
+import {router, useForm, usePage} from "@inertiajs/vue3";
+import PostModal from "@/Components/app/PostModal.vue";
 
-const postCreating = ref(false)
-const newPostForm = useForm({
-    body: ''
+const authUser = usePage().props.auth.user;
+const showModal = ref(false)
+const newPost = ref({
+    id: null,
+    body: '',
+    user: authUser
 })
 
-function submit() {
-    newPostForm.post(route('post.create'), {
-        onSuccess: () => {
-            newPostForm.reset()
-        }
-    })
+function showCreatePostModal() {
+    showModal.value = true
 }
 </script>
 
 <template>
     <div class="p-4 bg-white rounded-lg border mb-4">
-        <InputTextarea
-            @click="postCreating = true"
-            class="w-full mb-3"
-            placeholder="Click here to create new post"
-            rows="1"
-            v-model="newPostForm.body"
-        />
         <div
-            v-if="postCreating"
-            class="flex gap-2 justify-between">
-            <button
-                type="button"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 relative">
-                attach files
-                <input
-                    type="file"
-                    class="absolute left-0 top-0 right-0 bottom-0 opacity-0">
-            </button>
-            <button
-                @click="submit"
-                type="submit"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                submit
-            </button>
+            @click="showCreatePostModal"
+            class="py-2 px-3 border-2 border-gray-200 text-gray-500 rounded-md w-full mb-3">
+            Click here to create new post
         </div>
+<!--        <div-->
+<!--            class="flex gap-2 justify-between">-->
+<!--            <button-->
+<!--                type="button"-->
+<!--                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 relative">-->
+<!--                attach files-->
+<!--                <input-->
+<!--                    type="file"-->
+<!--                    class="absolute left-0 top-0 right-0 bottom-0 opacity-0">-->
+<!--            </button>-->
+<!--            <button-->
+<!--                @click="submit"-->
+<!--                type="submit"-->
+<!--                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">-->
+<!--                submit-->
+<!--            </button>-->
+<!--        </div>-->
+        <PostModal :post="newPost" v-model="showModal" />
     </div>
 </template>
 
--- resources/js/Components/app/PostModal.vue
+++ resources/js/Components/app/PostModal.vue
@@ -1,65 +1,66 @@
 <template>
     <teleport to="body">
         <TransitionRoot appear :show="show" as="template">
-        <Dialog as="div" @close="closeModal" class="relative z-10">
-            <TransitionChild
-                as="template"
-                enter="duration-300 ease-out"
-                enter-from="opacity-0"
-                enter-to="opacity-100"
-                leave="duration-200 ease-in"
-                leave-from="opacity-100"
-                leave-to="opacity-0"
-            >
-                <div class="fixed inset-0 bg-black/25" />
-            </TransitionChild>
-
-            <div class="fixed inset-0 overflow-y-auto">
-                <div
-                    class="flex min-h-full items-center justify-center p-4 text-center"
+            <Dialog as="div" @close="closeModal" class="relative z-10">
+                <TransitionChild
+                    as="template"
+                    enter="duration-300 ease-out"
+                    enter-from="opacity-0"
+                    enter-to="opacity-100"
+                    leave="duration-200 ease-in"
+                    leave-from="opacity-100"
+                    leave-to="opacity-0"
                 >
-                    <TransitionChild
-                        as="template"
-                        enter="duration-300 ease-out"
-                        enter-from="opacity-0 scale-95"
-                        enter-to="opacity-100 scale-100"
-                        leave="duration-200 ease-in"
-                        leave-from="opacity-100 scale-100"
-                        leave-to="opacity-0 scale-95"
-                    >
-                        <DialogPanel
-                            class="w-full max-w-md transform overflow-hidden rounded bg-white text-left align-middle shadow-xl transition-all"
-                        >
-                            <DialogTitle
-                                as="h3"
-                                class="flex items-center justify-between py-3 px-4 font-medium bg-gray-100 text-gray-900"
-                            >
-                                Update post
-                                <button @click="show = false" class="w-8 h-8 rounded-full hover:bg-black/5 transition flex items-center justify-center">
-                                    <XMarkIcon class="w-4 h-4"/>
-                                </button>
-                            </DialogTitle>
-                            <div class="p-4">
-                                <PostUserHeader :post="post" :show-time="false" class="mb-4"/>
-                                <ckeditor :editor="editor" v-model="form.body" :config="editorConfig"></ckeditor>
-<!--                                <InputTextarea v-model="form.body" class="mb-3 w-full" />-->
-                            </div>
+                    <div class="fixed inset-0 bg-black/25"/>
+                </TransitionChild>
 
-                            <div class="py-3 px-4">
-                                <button
-                                    type="button"
-                                    class="w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                    @click="submit"
+                <div class="fixed inset-0 overflow-y-auto">
+                    <div
+                        class="flex min-h-full items-center justify-center p-4 text-center"
+                    >
+                        <TransitionChild
+                            as="template"
+                            enter="duration-300 ease-out"
+                            enter-from="opacity-0 scale-95"
+                            enter-to="opacity-100 scale-100"
+                            leave="duration-200 ease-in"
+                            leave-from="opacity-100 scale-100"
+                            leave-to="opacity-0 scale-95"
+                        >
+                            <DialogPanel
+                                class="w-full max-w-md transform overflow-hidden rounded bg-white text-left align-middle shadow-xl transition-all"
+                            >
+                                <DialogTitle
+                                    as="h3"
+                                    class="flex items-center justify-between py-3 px-4 font-medium bg-gray-100 text-gray-900"
                                 >
-                                    Submit
-                                </button>
-                            </div>
-                        </DialogPanel>
-                    </TransitionChild>
+                                    {{form.id ? 'Update post' : 'Create post'}}
+                                    <button @click="show = false"
+                                            class="w-8 h-8 rounded-full hover:bg-black/5 transition flex items-center justify-center">
+                                        <XMarkIcon class="w-4 h-4"/>
+                                    </button>
+                                </DialogTitle>
+                                <div class="p-4">
+                                    <PostUserHeader :post="post" :show-time="false" class="mb-4"/>
+                                    <ckeditor :editor="editor" v-model="form.body" :config="editorConfig"></ckeditor>
+                                    <!--                                <InputTextarea v-model="form.body" class="mb-3 w-full" />-->
+                                </div>
+
+                                <div class="py-3 px-4">
+                                    <button
+                                        type="button"
+                                        class="w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                        @click="submit"
+                                    >
+                                        Submit
+                                    </button>
+                                </div>
+                            </DialogPanel>
+                        </TransitionChild>
+                    </div>
                 </div>
-            </div>
-        </Dialog>
-    </TransitionRoot>
+            </Dialog>
+        </TransitionRoot>
     </teleport>
 </template>
 
@@ -108,12 +109,23 @@ function closeModal() {
 }
 
 function submit() {
-    form.put(route('post.update', props.post.id), {
-        preserveScroll: true,
-        onSuccess: () => {
-            show.value = false
-        }
-    })
+    if (form.id) {
+        form.put(route('post.update', props.post.id), {
+            preserveScroll: true,
+            onSuccess: () => {
+                show.value = false
+                form.reset()
+            }
+        })
+    } else {
+        form.post(route('post.create'), {
+            preserveScroll: true,
+            onSuccess: () => {
+                show.value = false
+                form.reset()
+            }
+        })
+    }
 }
 
 </script>
